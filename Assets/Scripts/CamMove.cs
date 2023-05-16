ca31131339a9ea9762f9ab778c5f24a9fd80222e
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-05-16 23:41:39 +0900

    캐릭터 기본 움직임 구현 및 에셋 추가
    ㅇㅇ

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    [SerializeField] private CharMove charmove;
+    [SerializeField] private GameObject Target;
+    private float xRotate, yRotate, xRotateMove, yRotateMove;
+    public float rotateSpeed;
+
+    private void Start()
+    {
+        rotateSpeed = charmove.rotateSpeed;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(1)) // 클릭한 경우
+        {
+            xRotateMove = -Input.GetAxis("Mouse Y") * Time.deltaTime * rotateSpeed;
+            yRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+
+            yRotate = transform.eulerAngles.y + yRotateMove;
+            //xRotate = transform.eulerAngles.x + xRotateMove; 
+
+            xRotate = xRotate + xRotateMove;
+
+            xRotate = Mathf.Clamp(xRotate, -90, 90); // 위, 아래 고정
+
+            transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+        }
+    }
+    private void LateUpdate()
+    {
+        transform.position = Target.transform.position;
+    }
+}
---
+++ Assets/Scripts/CharMove.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 60.0f;
+    [SerializeField] private float jumpPower = 4;
+    public float rotateSpeed = 500.0f;
+    private Rigidbody myRigid;
+    [SerializeField] private bool isJumping = false;
+
+    private void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        /* player 이동 */
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = transform.forward * v + transform.right * h;
+        Vector3 moveAmount = moveDir.normalized * moveSpeed * Time.deltaTime;
+        myRigid.MovePosition(transform.position + moveAmount);
+        /* 회전 */
+        if (Input.GetMouseButton(1))
+        {
+            float yRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+
+            float yRotate = transform.eulerAngles.y + yRotateMove;
+
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotate, 0);
+        }
+
+        if (myRigid.velocity.y < 0) // 플레이어가 낙하중일 때 == velocity.y가 음수
+        {
+            Debug.DrawRay(myRigid.position, Vector3.down, Color.red); //ray를 그리기
+            if (Physics.Raycast(myRigid.position, Vector3.down, 0.2f))
+            {
+                isJumping = false;
+            }
+        }
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            isJumping = true;
+            myRigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+}
