19dcc6fae3b09a89ca738ae0998a8130c8d71cff
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-05-31 08:53:56 +0900

    싱글톤 변경
    ㅇㅇ

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,12 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+
+[DisallowMultipleComponent]
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
-    public T instance { get; protected set; } = null;
+    public bool dontDestroyOnLoad;
+
+    private static T _instance;
+    private static object _syncRoot = new object();
+
+    public static T instance
+    {
+        get
+        {
+            Initialize();
+            return _instance;
+        }
+    }
+
+    public static bool IsInitialized => _instance != null;
+
+    public static void Initialize()
+    {
+        if (IsInitialized)
+        {
+            return;
+        }
+        lock (_syncRoot)
+        {
+            _instance = FindObjectOfType<T>();
+
+            if (!IsInitialized)
+            {
+                var go = new GameObject(typeof(T).FullName);
+                _instance = go.AddComponent<T>();
+            }
+        }
+    }
+
     protected virtual void Awake()
     {
-        if (instance != null && instance != this) { Destroy(gameObject); return; }
-        instance = GetComponent<T>();
+        if (IsInitialized)
+        {
+            Debug.LogError(GetType().Name + " Singleton class is already created.");
+            Destroy(gameObject);
+            return;
+        }
+        Initialize();
+
+        if (dontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
     }
 }
