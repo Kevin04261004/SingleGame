9e1979d8d83cc062be3fd1befd7951a3879acb25
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-05-31 08:54:23 +0900

    UIManager 추가

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton <UIManager>
+{
+    [SerializeField] private TextMeshProUGUI timeClock_TMP;
+    [SerializeField] private Image RuleBook_BackGround;
+    public void Set_TimeClock_TMP(int time)
+    {
+        int hour = time / 60;
+        int minute = time % 60;
+        timeClock_TMP.text = string.Format("{0:D2} : {1:D2}", hour, minute);
+    }
+    public void Set_RuleBook_BackGround_TrueOrFalse()
+    {
+        if(RuleBook_BackGround.gameObject.activeSelf)
+        {
+            RuleBook_BackGround.gameObject.SetActive(false);
+        }
+        else
+        {
+            RuleBook_BackGround.gameObject.SetActive(true);
+        }
+    }
+}
