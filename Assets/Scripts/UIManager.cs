2093bbd580fa30f8da2ec45de2dab216a56dd653
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-05-31 19:58:38 +0900

    dd
    dd

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 
 public class UIManager : Singleton <UIManager>
 {
-    [SerializeField] private TextMeshProUGUI timeClock_TMP;
+    [SerializeField] private Text timeClock_text;
     [SerializeField] private Image RuleBook_BackGround;
     [SerializeField] private Image CCTV_BackGround;
     public void Set_TimeClock_TMP(int time)
     {
         int hour = time / 60;
         int minute = time % 60;
-        timeClock_TMP.text = string.Format("{0:D2} : {1:D2}", hour, minute);
+        timeClock_text.text = string.Format("{0:D2} : {1:D2}", hour, minute);
     }
     public void Set_RuleBook_BackGround_TrueOrFalse()
     {
