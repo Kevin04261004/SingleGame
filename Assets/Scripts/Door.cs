2e690780e9e5deffaf5f12443054dbe93867fd86
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-05-31 10:42:25 +0900

    문 열기 스크립트
    ㅇㅇ

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private bool isOpened = false;
+    [SerializeField] private Quaternion openedRotation;
+    [SerializeField] private Quaternion closedRotation;
+    [SerializeField] private float doorSpeed;
+
+    private Coroutine doorCoroutine;
+
+    private void Start()
+    {
+        closedRotation = transform.rotation;
+    }
+
+    public void ToggleDoor()
+    {
+        if (doorCoroutine != null)
+            StopCoroutine(doorCoroutine);
+
+        doorCoroutine = StartCoroutine(OpenCloseDoor());
+    }
+
+    private IEnumerator OpenCloseDoor()
+    {
+        Quaternion targetRotation = isOpened ? closedRotation : openedRotation;
+        Quaternion startRotation = transform.rotation;
+        float startTime = Time.time;
+        float endTime = startTime + doorSpeed;
+
+        while (Time.time < endTime)
+        {
+            float t = (Time.time - startTime) / doorSpeed;
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+        isOpened = !isOpened;
+    }
+}
